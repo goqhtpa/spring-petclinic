d838ab9fd8eae9d25bf8c0088c1d03bc08eba753
goqhtpa <153583023+goqhtpa@users.noreply.github.com>
2024-03-06 15:50:57 +0900

    Update kill_process.sh

--- deploy/scripts/kill_process.sh
+++ deploy/scripts/kill_process.sh
@@ -3,4 +3,5 @@
 echo "Remove existed container"
 
 ## 이전에 실행되는 컨테이너 종료
-docker-compose -f /home/ubuntu/deploy/docker-compose.yml down || true
+cd /home/ubuntu/deploy
+docker-compose -f docker-compose.yml down || true
